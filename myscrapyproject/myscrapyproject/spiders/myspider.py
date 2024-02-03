f475e29568374f5fbcb8223669c4ff16bad9bf7c
areda <v-areda@expediagroup.com>
2024-02-03 21:44:09 +0100

    initial commit

---
+++ myscrapyproject/myscrapyproject/spiders/myspider.py
@@ -0,0 +1,21 @@
+import scrapy
+
+
+class MyspiderSpider(scrapy.Spider):
+    name = "myspider"
+    allowed_domains = ["en.wikipedia.org"]
+    start_urls = ["https://en.wikipedia.org/wiki/Python_(programming_language)"]
+
+    def parse(self, response):
+        title = response.css('title').get()
+
+        content = response.css('div#mw-content-text > div.mw-content-ltr.mw-parser-output > p:nth-child(6)').get()
+
+        # Display the extracted data
+        self.log(f'Title: {title}')
+        self.log(f'Content: {content}')
+
+        yield {
+            'title': title,
+            'content': content,
+        }
